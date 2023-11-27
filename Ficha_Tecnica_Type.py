f6f5082bdc2becba7fd0ce442c581ec244e09cd9
VNCprojetos <vitorianublia@hotmail.com>
2023-11-27 20:42:12 -0300

    Exercicios e Projetos
    Testes

---
+++ Ficha_Tecnica_Type.py
@@ -0,0 +1,8 @@
+#FICHA TECNICA DE UM TYPE
+
+A = input('Digite Algo aqui:')
+
+print('O tipo primitivo desse valor é:', type(A))
+print('Só tem espaços?', A.isspace())
+print('Só temos Caracteres maiusculos?:', A.isupper())
+print('É numerico?:', A.isnumeric())
---
+++ Interação_Soma_de_Numeros.py
@@ -0,0 +1,10 @@
+N1 = int(input('Digite um valor:'))
+N2 = int(input('Digite outro valor:'))
+S = N1 + N2
+
+#FORMAS DE PRINT A SEREM USADAS
+
+#print ('essa soma vale {}' .format (S))
+#print ('A  soma de', N1, 'e' N2 é igual a: {}' .format(S))
+print ('A soma de {} e {} é igual a: {}' .format(N1, N2, S))
+
